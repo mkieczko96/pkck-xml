da63852ed8d399ddfa39cb7fb4c2cf73d20b2f32
Kieczkowski, Mateusz <mateusz.kieczkowski@accenture.com>
2019-06-12 16:31:03 +0200

    Dodany serializer do odczyt i zapisu danych z/do pliku xml

---
+++ Zadanie 5/MVVM/MVVM/Model/DAL/Serializer.cs
@@ -0,0 +1,27 @@
+﻿namespace MVVM.Model.DAL
+{
+    using System.IO;
+    using System.Xml.Serialization;
+
+    public class Serializer
+    {
+        public T Deserialize<T> (string input) where T : class
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using(StringReader reader = new StringReader(input))
+            {
+                return (T)xmlSerializer.Deserialize(reader);
+            }
+        }
+
+        public string Serialize<T>(T ObjectToSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType());
+            using(StringWriter stringWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(stringWriter, ObjectToSerialize);
+                return stringWriter.ToString();
+            }
+        }
+    }
+}
--- Zadanie 5/MVVM/MVVM/View/MainWindow.xaml.cs
+++ Zadanie 5/MVVM/MVVM/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 
 namespace MVVM.View
 {
@@ -10,6 +11,14 @@ namespace MVVM.View
         public MainWindow()
         {
             InitializeComponent();
+
+
+            Model.DAL.Serializer ser = new Model.DAL.Serializer();
+
+            string inputData = File.ReadAllText(@"C:\Users\mateusz.kieczkowski\OneDrive - Accenture\Desktop\pkck-xml\Zadanie 5\MVVM\MVVM\Database\helper.xml");
+
+            Model.BookStore bs = ser.Deserialize<Model.BookStore>(inputData);
+
         }
     }
 }
